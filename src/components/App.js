3c4ed22d1979cc22ac48a3020b5fca2cc5623e6a
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-12-23 19:44:06 -0500

    All tests passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, {useState, useEffect} from "react"
+
+function App(){
+
+    const [dog, setDog] = useState("")
+
+    useEffect(()=>{
+
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then(data => setDog(data.message))
+
+    }, [])
+
+    return(
+        <div>
+            <h1>free doggo pic</h1>
+            {dog === ""
+            ? <p>Loading . . .</p>
+            :<img src={dog} alt="A Random Dog" />}
+
+        </div>
+    )
+}
+
+export default App;
